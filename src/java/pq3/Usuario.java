9c6144ba1dc3d08bfe0dcad0d646b23e1c65823a
santiagob20 <santiago.barrera20@gmail.com>
2018-03-09 23:10:12 -0500

    carga incial del proyecto

---
+++ src/java/pq3/Usuario.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pq3;
+
+/**
+ *
+ * @author santiagob20
+ */
+public class Usuario {
+    private String user;
+    private String password;
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
